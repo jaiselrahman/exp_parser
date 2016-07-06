287c0c16944f56e0298f069fd31aafa1157094e9
jaisel <jaisel20@gmail.com>
2016-07-06 13:33:29 +0530

    added arbitary percision support

--- include/exp_version.h
+++ include/exp_version.h
@@ -1,5 +1,5 @@
-#define exp_VERSION "3.0.0"
+#define exp_VERSION "3.1.0"
 #define exp_VERSION_MAJOR "3"
-#define exp_VERSION_MINOR "0"
+#define exp_VERSION_MINOR "1"
 #define exp_VERSION_PATCH "0"
 #define exp_VERSION_TWEAK ""
